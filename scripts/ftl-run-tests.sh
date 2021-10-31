2fd9f60e93f073abb46de637c5b5172cb0f9d69b
Rafael Toledo <rafaeldtoledo@gmail.com>
2021-10-31 20:58:05 -0300

    Migrate CI to Github Actions

--- scripts/ftl-run-tests.sh
+++ scripts/ftl-run-tests.sh
@@ -3,8 +3,8 @@
 # Run tests on test lab
 gcloud firebase test android run \
     --type instrumentation \
-    --app app/build/outputs/apk/debug/app-debug.apk \
-    --test app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+    --app debug-apk/app-debug.apk \
+    --test test-apk/app-debug-androidTest.apk \
     --device model=Nexus6P,version=27,locale=en_US,orientation=portrait \
     --timeout 30m \
     --results-bucket cloud-test-social-app-development \
